561b948747759aaa9cca40ab0cf157ca5bf3f011
NABIL AHMED <5687nabil@gmail.com>
2024-03-01 16:38:13 +0600

    Create example.py

---
+++ random-username/example.py
@@ -0,0 +1,6 @@
+from generate_random_username import get_random_username
+
+firstname = input("Enter your firstname: ")
+lastname = input("Enter your lastname: ") 
+
+print(get_random_username(firstname=firstname, lastname=lastname))
